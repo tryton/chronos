98c063a4e28a45856588f0fd271fe1259877db5d
Cédric Krier <ced@b2ck.com>
2019-08-25 12:34:00 +0200

    Use PNG icons
    Chromium does not support SVG format.
    So we convert the SVG into PNG in the build for the recommended sizes.
    We keep using SVG for theme_icons as it is not used by Chromium.
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=29683
    issue8583
    review249781002

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 rm -f chronos.zip
+for icon in icons/*.svg; do
+    icon=`basename $icon`
+    for size in 16 48 128; do
+        convert -background none -resize $sizex$size icons/$icon icons/`basename $icon .svg`-$size.png
+    done
+done
 zip -r chronos.zip \
     manifest.* \
     *.html \
     *.css \
     *.js \
     icons/*.svg \
+    icons/*.png \
     bower_components/bootstrap/LICENSE \
     bower_components/bootstrap/dist/js/bootstrap.js \
     bower_components/bootstrap/dist/css/bootstrap.css \
